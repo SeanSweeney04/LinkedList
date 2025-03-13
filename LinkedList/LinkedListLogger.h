1a3434480d378fb1c8032294047c1c0b61d6eea7
Sean Sweeney <ssweeney1@corp.siriusxm.com>
2025-03-13 15:28:07 +0000

    Pull request 1

---
+++ LinkedList/LinkedListLogger.h
@@ -0,0 +1,17 @@
+#ifndef LINKEDLISTLOGGER_H
+#define LINKEDLISTLOGGER_H
+
+#include "LinkedList.h"
+#include <iostream>
+
+class LinkedListLogger 
+{
+public:
+    explicit LinkedListLogger(LinkedList& list);  
+    void print(); 
+
+private:
+    LinkedList& list;  
+};
+
+#endif
